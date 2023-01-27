aeffea316d5959e6ff998cef0cde443d0b72820c
Divyansh Mishra <88994556+meshhra@users.noreply.github.com>
2023-01-27 22:39:28 +0530

    Player Controller's Jump finalized

--- platformer remade/Assets/Scripts/Movement/PlayerController.cs
+++ platformer remade/Assets/Scripts/Movement/PlayerController.cs
@@ -108,15 +108,10 @@ public class PlayerController : MonoBehaviour
     }
     #endregion
 
-
     #region CIELING CHECK
     [Header("CIELING CHECK")]
     [SerializeField] private bool isCieled = false;
-    /*
-    [SerializeField] private LayerMask layerMask;
-    [SerializeField] private float rayLength = 1f;
-    [SerializeField] private float raySpacing;
-    [SerializeField] private int rayCount = 4;*/
+    
 
     void CheckCieling()
     {
@@ -182,73 +177,23 @@ public class PlayerController : MonoBehaviour
 
     #region JUMPING
     [Header("JUMPING")]
-    /*
-    [SerializeField] private float jumpHeight = 40f;
-    public bool jumping;
-    //public float _jumpAcc = 80f;
-    void CalculateJump()
-    {
-        if(jumpDown && isGrounded)
-        {
-            rigiBody2D.velocity = new Vector2(rigiBody2D.velocity.x, jumpHeight);
-        }
-        if(jumpUp && rigiBody2D.velocity.y > 0)
-        {
-            rigiBody2D.velocity = new Vector2(rigiBody2D.velocity.x, 0);
-        }
-    }*/
-    [SerializeField]private float _defaultJumpDuration = 5f;
-    [SerializeField] private float _jumpVelocity = 10f;
-    [SerializeField] private float _jumpHeight = 6f;
-    [SerializeField] private bool _jumping = false;
     
-
+    [SerializeField] private bool _jumping = false;
     [SerializeField]private float _jumpDuration;
     [SerializeField] private AnimationCurve curve;
-    /*void CalculateJump()
-    {
-        if(isGrounded && jumpDown)
-        { 
-            _jumpDuration = _defaultJumpDuration;
-        }
-
-        if(isCieled)
-        {
-            _jumpDuration = 0;
-            //rigiBody2D.velocity = new Vector2(rigiBody2D.velocity.x, 0f);
-        }
-
-        if(!isGrounded && rigiBody2D.velocity.y >0 && jumpUp)
-        {
-            _jumpDuration = 0;
-            rigiBody2D.velocity = new Vector2(rigiBody2D.velocity.x, 0);
-        }
-
-        if(_jumpDuration > 0)
-        {
-            _jumpDuration -= Time.deltaTime;
-            rigiBody2D.velocity = new Vector2(rigiBody2D.velocity.x, _jumpVelocity);
-        }
-    }*/
-
-    //this is while copying hollowknight
+    
     public float time = 0f;
-    //public float factor = 90f;
-    //[Range(0.1f, 1f)] public float x; 
-
-    public float y = 0; //this is the variable for lerp
     public bool _airbone;
-
     public float _airTime;
     void CalculateJump()
     {
         if(jumpDown && isGrounded)
         {
             time = 0;
-           // y = 0;
+           
             _airTime = 0;
             _jumping = true;
-            //currentVerticalSpeed = _jumpVelocity;
+            
         }
 
         if(_jumping && !isGrounded)
@@ -259,23 +204,6 @@ public class PlayerController : MonoBehaviour
 
         if(_jumping)
         {
-            /*time += Time.deltaTime;
-            if(time < _jumpDuration/2)
-            {
-                //currentVerticalSpeed -= Time.deltaTime * factor;
-                //currentVerticalSpeed = Mathf.Clamp(currentVerticalSpeed, _jumpVelocity*x, _jumpVelocity);
-                currentVerticalSpeed = Mathf.Lerp(_jumpVelocity, 0, _jumpDuration * Time.deltaTime);
-                rigiBody2D.velocity = new Vector2(rigiBody2D.velocity.x, currentVerticalSpeed);
-            }
-            else
-            {
-                //rigiBody2D.velocity = new Vector2(rigiBody2D.velocity.x, 0);
-                _jumping = false;
-
-            }*/
-            
-            //y += Time.deltaTime;
-            //currentVerticalSpeed = curve.Evaluate(y);
             if (time < _jumpDuration / 2) 
             {
                 time += Time.deltaTime;
@@ -284,17 +212,22 @@ public class PlayerController : MonoBehaviour
             }
         }
 
-        if(rigiBody2D.velocity.y > 0 && jumpUp)
+        if(_jumping && rigiBody2D.velocity.y > 0 && jumpUp)
         {
             _jumping = false;
             rigiBody2D.velocity = new Vector2(rigiBody2D.velocity.x, 0);
         }
 
-        if(_airbone)
+        if(_jumping && isCieled && rigiBody2D.velocity.y>0)
+        {
+            _jumping = false;
+        }
+
+        if (_airbone)
         {
             _airTime += Time.deltaTime;
         }
-        if(_airbone && isGrounded) //&& rigiBody2D.velocity.y == 0)
+        if(_airbone && isGrounded)
         {
             _airbone = false;
             _jumping = false;
