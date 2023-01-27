e36babcf7371526054d9b85fb5ea3d67c8187caf
Divyansh Mishra <88994556+meshhra@users.noreply.github.com>
2023-01-28 00:36:01 +0530

    somehow jump buferign works

--- platformer remade/Assets/Scripts/Movement/PlayerController.cs
+++ platformer remade/Assets/Scripts/Movement/PlayerController.cs
@@ -53,11 +53,11 @@ public class PlayerController : MonoBehaviour
 
     void SetHorizonatlSpeed()
     {
-        if (inputX!=0)
+        if (inputX != 0)
         {
             //currentHorizontalSpeed = Mathf.MoveTowards(currentHorizontalSpeed, movementSpeed * inputX, acceleration * Time.deltaTime);
             currentHorizontalSpeed += Time.deltaTime * movementSpeed * acceleration * inputX;
-            currentHorizontalSpeed = Mathf.Clamp(currentHorizontalSpeed,-movementSpeed,movementSpeed);
+            currentHorizontalSpeed = Mathf.Clamp(currentHorizontalSpeed, -movementSpeed, movementSpeed);
         }
         else
         {
@@ -82,8 +82,8 @@ public class PlayerController : MonoBehaviour
         Vector2 _rayOrigin1 = boxCollider2D.bounds.min + (Vector3.right * raySpacing * 1);
         Vector2 _rayOrigin2 = boxCollider2D.bounds.min + (Vector3.right * raySpacing * 2);
         Vector2 _rayOrigin3 = boxCollider2D.bounds.min + (Vector3.right * raySpacing * 3);
-        
-        
+
+
         RaycastHit2D hit0 = Physics2D.Raycast(_rayOrigin0, Vector2.down, rayLength, layerMask);
         RaycastHit2D hit1 = Physics2D.Raycast(_rayOrigin1, Vector2.down, rayLength, layerMask);
         RaycastHit2D hit2 = Physics2D.Raycast(_rayOrigin2, Vector2.down, rayLength, layerMask);
@@ -111,7 +111,7 @@ public class PlayerController : MonoBehaviour
     #region CIELING CHECK
     [Header("CIELING CHECK")]
     [SerializeField] private bool isCieled = false;
-    
+
 
     void CheckCieling()
     {
@@ -149,13 +149,13 @@ public class PlayerController : MonoBehaviour
     #region CALCULATING GRAVITY
     [Header("GRAVITY")]
     [SerializeField] private float defaultGravity = 1f;
-    [SerializeField][Range(1,50)] private float gravityUp = 15f;
-    [SerializeField][Range(1,50)] private float gravityDown = 20f;
+    [SerializeField][Range(1, 50)] private float gravityUp = 15f;
+    [SerializeField][Range(1, 50)] private float gravityDown = 20f;
     [SerializeField] private float fallCap = 30f;
     void CalculateGravity()
     {
         if (rigiBody2D.velocity.y > 0)
-        { 
+        {
             rigiBody2D.gravityScale = gravityUp;
         }
         else if (rigiBody2D.velocity.y < 0)
@@ -171,29 +171,51 @@ public class PlayerController : MonoBehaviour
         {
             rigiBody2D.velocity = new Vector2(rigiBody2D.velocity.x, -fallCap);
         }
-        
+
     }
     #endregion
 
     #region JUMPING
     [Header("JUMPING")]
-    
+
     [SerializeField] private bool _jumping = false;
-    [SerializeField]private float _jumpDuration;
-    [SerializeField] private AnimationCurve curve;
+    [SerializeField] private float _jumpDuration;
+    [SerializeField] private float _jumpBuffer = 1f;
+
     
-    public float time = 0f;
+    [SerializeField] private AnimationCurve curve;
+    [SerializeField] private bool startBufferTimer;
+    [SerializeField] private bool isJumpbuffered = false;
+    [SerializeField] private float timerForJump = 0f;
+    [SerializeField] private float timerForJumpBuffer = 0;
     public bool _airbone;
     public float _airTime;
     void CalculateJump()
     {
-        if(jumpDown && isGrounded)
+        if(jumpDown)
         {
-            time = 0;
-           
-            _airTime = 0;
-            _jumping = true;
+            if(_airbone)
+            {
+                startBufferTimer = true;
+            }
+            if(isGrounded)
+            {
+                _jumping = true;
+                isJumpbuffered = false;
+                timerForJump = 0;
+                timerForJumpBuffer = 0;
+                _airTime = 0;
+            }
             
+        }
+        
+        if(isJumpbuffered)
+        {
+            _jumping = true;
+            isJumpbuffered = false;
+            timerForJump = 0;
+            timerForJumpBuffer = 0;
+            _airTime = 0;
         }
 
         if(_jumping && !isGrounded)
@@ -204,21 +226,27 @@ public class PlayerController : MonoBehaviour
 
         if(_jumping)
         {
-            if (time < _jumpDuration / 2) 
+            if (timerForJump < _jumpDuration / 2) 
             {
-                time += Time.deltaTime;
-                currentVerticalSpeed = curve.Evaluate(time);
+                timerForJump += Time.deltaTime;
+                currentVerticalSpeed = curve.Evaluate(timerForJump);
                 rigiBody2D.velocity = new Vector2(rigiBody2D.velocity.x, currentVerticalSpeed); 
             }
         }
-
+        
         if(_jumping && rigiBody2D.velocity.y > 0 && jumpUp)
         {
             _jumping = false;
+            
             rigiBody2D.velocity = new Vector2(rigiBody2D.velocity.x, 0);
         }
 
-        if(_jumping && isCieled && rigiBody2D.velocity.y>0)
+        if (jumpUp && rigiBody2D.velocity.y < 0)
+        {
+            _jumping = false;
+        }
+
+        if (_jumping && isCieled && rigiBody2D.velocity.y>0)
         {
             _jumping = false;
         }
@@ -230,7 +258,19 @@ public class PlayerController : MonoBehaviour
         if(_airbone && isGrounded)
         {
             _airbone = false;
-            _jumping = false;
+            //_jumping = false;
+            startBufferTimer = false;
+
+        }
+
+        if(isGrounded && (timerForJumpBuffer < _jumpBuffer&&timerForJumpBuffer > 0))
+        {
+            isJumpbuffered = true;
+        }
+
+        if(startBufferTimer)
+        {
+            timerForJumpBuffer += Time.deltaTime;
         }
     }
 
