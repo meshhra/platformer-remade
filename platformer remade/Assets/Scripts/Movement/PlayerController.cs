ee04f27f9e09256a401ec4f30109f6b25f72bc49
Divyansh Mishra <88994556+meshhra@users.noreply.github.com>
2023-01-27 20:34:30 +0530

    some chnges

--- platformer remade/Assets/Scripts/Movement/PlayerController.cs
+++ platformer remade/Assets/Scripts/Movement/PlayerController.cs
@@ -1,4 +1,5 @@
 
+using System;
 using UnityEngine;
 using static UnityEngine.UI.Image;
 
@@ -196,11 +197,13 @@ public class PlayerController : MonoBehaviour
     }*/
     [SerializeField]private float _defaultJumpDuration = 5f;
     [SerializeField] private float _jumpVelocity = 10f;
+    [SerializeField] private float _jumpHeight = 6f;
     [SerializeField] private bool _jumping = false;
     
 
     [SerializeField]private float _jumpDuration;
-    void CalculateJump()
+    [SerializeField] private AnimationCurve curve;
+    /*void CalculateJump()
     {
         if(isGrounded && jumpDown)
         { 
@@ -223,6 +226,76 @@ public class PlayerController : MonoBehaviour
         {
             _jumpDuration -= Time.deltaTime;
             rigiBody2D.velocity = new Vector2(rigiBody2D.velocity.x, _jumpVelocity);
+        }
+    }*/
+
+    //this is while copying hollowknight
+    public float time = 0f;
+    //public float factor = 90f;
+    //[Range(0.1f, 1f)] public float x; 
+
+    public float y = 0; //this is the variable for lerp
+    public bool _airbone;
+
+    public float _airTime;
+    void CalculateJump()
+    {
+        if(jumpDown && isGrounded)
+        {
+            time = 0;
+           // y = 0;
+            _airTime = 0;
+            _jumping = true;
+            //currentVerticalSpeed = _jumpVelocity;
+        }
+
+        if(_jumping && !isGrounded)
+        {
+            _airbone = true;
+            
+        }
+
+        if(_jumping)
+        {
+            /*time += Time.deltaTime;
+            if(time < _jumpDuration/2)
+            {
+                //currentVerticalSpeed -= Time.deltaTime * factor;
+                //currentVerticalSpeed = Mathf.Clamp(currentVerticalSpeed, _jumpVelocity*x, _jumpVelocity);
+                currentVerticalSpeed = Mathf.Lerp(_jumpVelocity, 0, _jumpDuration * Time.deltaTime);
+                rigiBody2D.velocity = new Vector2(rigiBody2D.velocity.x, currentVerticalSpeed);
+            }
+            else
+            {
+                //rigiBody2D.velocity = new Vector2(rigiBody2D.velocity.x, 0);
+                _jumping = false;
+
+            }*/
+            
+            //y += Time.deltaTime;
+            //currentVerticalSpeed = curve.Evaluate(y);
+            if (time < _jumpDuration / 2) 
+            {
+                time += Time.deltaTime;
+                currentVerticalSpeed = curve.Evaluate(time);
+                rigiBody2D.velocity = new Vector2(rigiBody2D.velocity.x, currentVerticalSpeed); 
+            }
+        }
+
+        if(rigiBody2D.velocity.y > 0 && jumpUp)
+        {
+            _jumping = false;
+            rigiBody2D.velocity = new Vector2(rigiBody2D.velocity.x, 0);
+        }
+
+        if(_airbone)
+        {
+            _airTime += Time.deltaTime;
+        }
+        if(_airbone && isGrounded) //&& rigiBody2D.velocity.y == 0)
+        {
+            _airbone = false;
+            _jumping = false;
         }
     }
 
