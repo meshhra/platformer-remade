3e1b41a25c69f5ed84a9af2ae1d3589ce91f4f5f
Divyansh Mishra <88994556+meshhra@users.noreply.github.com>
2023-04-17 16:56:13 +0530

    killed the player when touch spike

--- platformer remade/Assets/Scripts/tests/CheckSpikes.cs
+++ platformer remade/Assets/Scripts/tests/CheckSpikes.cs
@@ -5,11 +5,24 @@ using UnityEngine;
 
 public class CheckSpikes : MonoBehaviour
 {
+
+    public bool isPlayerDead = false;
+
+    public GameObject player;
+
+    private void Start()
+    {
+        player = GameObject.Find("Player");
+    }
+
     private void OnCollisionEnter2D(Collision2D col)
     {
+        
         if (col.collider.gameObject.CompareTag("spike"))
         {
             Debug.Log("mrr gaya");
+            isPlayerDead = true;
+            Destroy(player);
         }
     }
 }
