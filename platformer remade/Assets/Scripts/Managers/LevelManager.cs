aea888e284605efbad8d2f537d9b431dd99f2737
Divyansh Mishra <88994556+meshhra@users.noreply.github.com>
2023-05-19 22:23:51 +0530

    made the transition

--- platformer remade/Assets/Scripts/Managers/LevelManager.cs
+++ platformer remade/Assets/Scripts/Managers/LevelManager.cs
@@ -1,4 +1,6 @@
 
+using System;
+using System.Collections;
 using GamePlay;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -8,17 +10,25 @@ namespace Managers
 {
     public class LevelManager : MonoBehaviour
     {
+       
+        
         [SerializeField] private PlayerDeath playerDeath;
+        private Rigidbody2D playerRigidBody2D;
         [SerializeField] private PlayerMoveLevels playerMoveLevels;
         [SerializeField] private bool isStartTimer;
         [SerializeField] private float timer;
 
+        [SerializeField] private Animator transitionAnimator;
+
         [FormerlySerializedAs("currentScene")] [SerializeField] private int currentSceneBuildIndex;
+
+        
 
         private void Start()
         {
             playerDeath = FindObjectOfType<PlayerDeath>();
             playerMoveLevels = FindObjectOfType<PlayerMoveLevels>();
+            playerRigidBody2D = playerDeath.gameObject.GetComponent<Rigidbody2D>();
 
             currentSceneBuildIndex = SceneManager.GetActiveScene().buildIndex;
         }
@@ -32,47 +42,21 @@ namespace Managers
         private void ReloadSceneWhenPlayerDies()
         {
             if (!playerDeath.IsPlayerDead) return;
-            if (!isStartTimer)
-            {
-                timer = 0;
-                isStartTimer = true;
-            }
-
-            switch (isStartTimer)
-            {
-                case true when timer < 1:
-                    timer += Time.deltaTime;
-                    break;
-                case true when timer > 1:
-                    isStartTimer = false;
-                    timer = 0;
-                    
-                    SceneManager.LoadScene(currentSceneBuildIndex);
-                    break;
-            }
+            StartCoroutine(LoadScene(currentSceneBuildIndex));
         }
 
         private void MovePlayerToNextLevel()
         {
             if (!playerMoveLevels.IsMoveToNextLevel) return;
-            if (!isStartTimer)
-            {
-                timer = 0;
-                isStartTimer = true;
-            }
+            StartCoroutine(LoadScene(currentSceneBuildIndex + 1));
+        }
 
-            switch (isStartTimer)
-            {
-                case true when timer < 0.5f:
-                    timer += Time.deltaTime;
-                    break;
-                case true when timer > 0.5f:
-                    isStartTimer = false;
-                    timer = 0;
-                    
-                    SceneManager.LoadScene(currentSceneBuildIndex + 1);
-                    break;
-            }
+        private IEnumerator LoadScene(int buildIndex)
+        {
+            transitionAnimator.Play("CrossFadeIn");
+            yield return new WaitForSeconds(1);
+            
+            SceneManager.LoadScene(buildIndex);
         }
     }
 }
