dda17ffcebb23010e92fd297ff071f817407fcd7
Divyansh Mishra <88994556+meshhra@users.noreply.github.com>
2023-05-28 08:30:26 +0530

    Made falling block

--- platformer remade/Assets/Scripts/GamePlay/FallingBlock.cs
+++ platformer remade/Assets/Scripts/GamePlay/FallingBlock.cs
@@ -1,17 +1,18 @@
 using System;
+using System.Collections;
 using UnityEngine;
 
 namespace GamePlay
 {
     public class FallingBlock : MonoBehaviour
     {
-        private Rigidbody2D blockRigidbody2D;
+        [SerializeField]private Rigidbody2D blockRigidbody2D;
         [SerializeField]private float gravityScale;
         private PlayerDeath playerDeath;
 
         private void Start()
         {
-            playerDeath = GameObject.Find("Player").GetComponent<PlayerDeath>();
+            playerDeath = GameObject.Find("Player Character").GetComponent<PlayerDeath>();
             blockRigidbody2D = GetComponent<Rigidbody2D>();
             blockRigidbody2D.gravityScale = 0;
 
@@ -19,8 +20,16 @@ namespace GamePlay
 
         }
 
+        public float waitTime = 10;
+
         private void StartFalling()
         {
+            StartCoroutine(FallingCoroutine());
+        }
+        
+        private IEnumerator FallingCoroutine()
+        {
+            yield return new WaitForSeconds(waitTime);
             blockRigidbody2D.gravityScale = gravityScale;
         }
     }
--- platformer remade/Assets/Scripts/GamePlay/PlayerDeath.cs
+++ platformer remade/Assets/Scripts/GamePlay/PlayerDeath.cs
@@ -21,10 +21,8 @@ namespace GamePlay
 
         private void OnTriggerEnter2D(Collider2D col)
         {
-            if (col.gameObject.CompareTag("Falling Block Trigger"))
-            {
-                OnPlayerEnterFallBlockTrigger?.Invoke();
-            }
+            if (!col.gameObject.CompareTag("fallingBlockTrigger")) return;
+            OnPlayerEnterFallBlockTrigger?.Invoke();
         }
     }
 }
